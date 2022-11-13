f939ef2aeecbc66e36b5338b180fb27d80988be6
Igor Moraes <igor.sgm@gmail.com>
2022-11-12 17:09:43 -0800

    HooksPipeline tests

---
+++ tests/Fixtures/HooksPipelineInvokableFixture3.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Igorsgm\GitHooks\Tests\Fixtures;
+
+class HooksPipelineInvokableFixture3
+{
+    /**
+     * Get hook name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'Hook 3';
+    }
+
+    public function __invoke($message, $next)
+    {
+        $message .= ' Hook 3';
+
+        return $next($message);
+    }
+}
